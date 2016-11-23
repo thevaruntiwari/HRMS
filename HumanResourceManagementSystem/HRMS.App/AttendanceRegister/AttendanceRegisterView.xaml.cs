fa991176aabf92017311809561a26eeae1727204
thevaruntiwari <thevaruntiwari@gmail.com>
2016-11-23 22:01:08 +0530

    Added views and view-models

---
+++ HumanResourceManagementSystem/HRMS.App/AttendanceRegister/AttendanceRegisterView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HRMS.App.AttendanceRegister
+{
+    /// <summary>
+    /// Interaction logic for AttendanceRegisterView.xaml
+    /// </summary>
+    public partial class AttendanceRegisterView : UserControl
+    {
+        public AttendanceRegisterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HumanResourceManagementSystem/HRMS.App/LeaveApplication/LeaveApplicationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.App.LeaveApplication
+{
+    class LeaveApplicationViewModel
+    {
+    }
+}
